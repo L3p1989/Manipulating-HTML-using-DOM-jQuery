8216c4bb7e5eb7a1844379e57d97b4cff72e94cc
Patrick Hogan <phogan110@live.com>
2018-10-21 07:08:36 -0600

    added `btnSubmitText` var; appended `btnSubmitText` to `btnSubmit`

--- DOM/script.js
+++ DOM/script.js
@@ -1,3 +1,5 @@
-var btnSubmit = document.createElement('button');
+var btnSubmit = document.createElement('button', 'type="submit"');
+var btnSubmitText = document.createTextNode("submit")
 
+btnSubmit.appendChild(btnSubmitText);
 document.body.appendChild(btnSubmit);
