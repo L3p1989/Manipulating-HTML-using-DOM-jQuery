d1a979a4c374a15deee1b2bb055aeaaa60ad559c
Patrick Hogan <phogan110@live.com>
2018-10-21 07:12:55 -0600

    added 'click' event to `btnSubmit`

--- DOM/script.js
+++ DOM/script.js
@@ -1,5 +1,9 @@
 var btnSubmit = document.createElement('button', 'type="submit"');
-var btnSubmitText = document.createTextNode("submit")
+var btnSubmitText = document.createTextNode("Submit")
 
 btnSubmit.appendChild(btnSubmitText);
 document.body.appendChild(btnSubmit);
+
+btnSubmit.addEventListener('click', function() {
+    alert('You clicked me!')
+})
