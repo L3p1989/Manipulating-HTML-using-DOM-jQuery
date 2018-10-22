000a5247dd7e16c99909c638dccf764eafdde122
Patrick Hogan <phogan110@live.com>
2018-10-22 06:36:55 -0600

    commented code and added var `myDiv`

--- DOM/script.js
+++ DOM/script.js
@@ -8,6 +8,12 @@ var colorDiv = document.getElementsByClassName('back-div')[0];
 var colorText = document.createElement('p')
 var ctText = document.createTextNode('Please click gently!')
 
+var myDiv = document.createElement('div')
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+};//this is a random number generator for 0-max
+
 inputSubmit.appendChild(inputSubmitText);//adds text to #inputSubmit
 
 inputSubmit.addEventListener('click', function() {
@@ -22,9 +28,9 @@ colorDiv.addEventListener('mouseleave', function() {
     colorDiv.style.backgroundColor = 'white'
 });//returns to white when mouse leaves colorDiv
 
-document.body.insertBefore(colorText, document.body.childNodes[7]);//inserts colorText before script
-colorText.appendChild(ctText)
+document.body.insertBefore(colorText, document.body.childNodes[7]);//inserts colorText before body index 7
+colorText.appendChild(ctText);//appends text to colorText
 
 colorText.addEventListener('click', function() {
-    colorText.style.color = "red"
-})
+    colorText.style.color = "rgb(" + getRandomInt(255) + ", " + getRandomInt(255) + ", " + getRandomInt(255) + ")";
+});//gives colorText a random text color when clicked
