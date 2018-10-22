d1003d9d3be91b3bbefb9d33c919012b468c5542
Patrick Hogan <phogan110@live.com>
2018-10-22 06:57:25 -0600

    added var `divBtn` with var `divBtnText`; added click event for `divBtn`

--- DOM/script.js
+++ DOM/script.js
@@ -8,6 +8,8 @@ var colorDiv = document.getElementsByClassName('back-div')[0];
 var colorText = document.createElement('p')
 var ctText = document.createTextNode('Please click gently!')
 
+var divBtn = document.createElement('button')
+var divBtnText = document.createTextNode('Me?')
 var myDiv = document.createElement('div')
 
 function getRandomInt(max) {
@@ -28,9 +30,22 @@ colorDiv.addEventListener('mouseleave', function() {
     colorDiv.style.backgroundColor = 'white'
 });//returns to white when mouse leaves colorDiv
 
-document.body.insertBefore(colorText, document.body.childNodes[7]);//inserts colorText before body index 7
+document.body.insertBefore(colorText, document.body.childNodes[7]);//creates colorText as body index 7
 colorText.appendChild(ctText);//appends text to colorText
 
 colorText.addEventListener('click', function() {
     colorText.style.color = "rgb(" + getRandomInt(255) + ", " + getRandomInt(255) + ", " + getRandomInt(255) + ")";
 });//gives colorText a random text color when clicked
+
+document.body.insertBefore(divBtn, document.body.childNodes[8]);//creates divBtn as body index 8
+divBtn.appendChild(divBtnText);
+
+document.body.insertBefore(myDiv, document.body.childNodes[9]);//creates myDiv as body index 9
+
+divBtn.addEventListener('click', function() {
+    var myNameSpace = document.createElement('span');
+    var myName = document.createTextNode('Patrick')
+
+    myDiv.appendChild(myNameSpace)
+    myNameSpace.appendChild(myName)
+});//adds myNameSpace with myName text to myDiv on divBtn click
