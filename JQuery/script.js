0e5674ec40dd648981c061f97917598af0c807c1
Patrick Hogan <phogan110@live.com>
2018-10-22 08:28:38 -0600

    created hover function for new div

--- JQuery/script.js
+++ JQuery/script.js
@@ -1,5 +1,19 @@
-$('body').prepend('<button type="submit">Click</button>');
+$('body').prepend('<button type="submit">Click</button>');//prepends first button
 
-$('button[type="submit"]').click(function() {
+$("button:contains('Click')").click(function() {
     alert('You clicked me!')
-});
+});//first button function is to alert that you clicked it
+
+$('button:contains("Submit")').click(function() {
+    if ($('input').val() == '') {
+        alert("You haven't typed anything")
+    } else {
+        alert($('input').val());
+    }
+});//submits input text as an alert if there's a value to display
+
+$("div:contains('Hey')").hover(function() {
+    $(this).css('background', 'purple');
+}, function() {
+    $(this).css('background', 'white');
+});//changes background color of div when moused over
