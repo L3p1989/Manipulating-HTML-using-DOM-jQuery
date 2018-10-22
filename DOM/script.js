ddf2086e80a1e16b066f14d8c590fecab5d13720
Patrick Hogan <phogan110@live.com>
2018-10-21 19:36:30 -0600

    corrected remaining code to still interact with html; set `#btnSubmit` 'click' function to alert value

--- DOM/script.js
+++ DOM/script.js
@@ -1,8 +1,11 @@
 var btnSubmit = document.getElementsByClassName('btnSubmit');
 var btnSubmitText = document.createTextNode("Submit");
 
+var textInput = document.getElementsByTagName('input')
+
 btnSubmit[0].appendChild(btnSubmitText);
 
 btnSubmit[0].addEventListener('click', function() {
-    alert()
+    alert(textInput[0].value)
 });
+
