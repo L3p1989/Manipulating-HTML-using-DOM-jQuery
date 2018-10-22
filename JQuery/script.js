24c299b531c3eff2d62b2f6b95a2947be04df436
Patrick Hogan <phogan110@live.com>
2018-10-22 07:39:12 -0600

    changed button text to click

--- JQuery/script.js
+++ JQuery/script.js
@@ -1,5 +1,5 @@
-$('body').prepend('<button type="submit">Submit</button>');
+$('body').prepend('<button type="submit">Click</button>');
 
 $('button[type="submit"]').click(function() {
     alert('You clicked me!')
-})
+});
