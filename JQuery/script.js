8f42f867929aba982036d24e2257a63b881772bb
Patrick Hogan <phogan110@live.com>
2018-10-22 09:07:48 -0600

    created 'click' function for `button`: 'Friends'

--- JQuery/script.js
+++ JQuery/script.js
@@ -2,6 +2,10 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 };//this is a random number generator for 0-max
 
+var friends = ['Steph', 'Julius', 'Glenn', 'Jason', 'Tyler', 'Saoirse', 'Mishka', 'Polka', 'Namira', 'Louie'];
+
+var friendCount = 0;
+
 $('body').prepend('<button type="submit">Click</button>');//prepends first button
 
 $("button:contains('Click')").click(function() {
@@ -28,10 +32,19 @@ $("p:contains('Click me')").click(function() {
     $(this).css('color', 'rgb(' + getRandomInt(255) + ',' + getRandomInt(255) + ',' + getRandomInt(255));
 });//adds 'click' function to `p`
 
-$("p:contains('Click me')").after('<button>Me?</button>')
+$("p:contains('Click me')").after('<button>Me?</button>');//adds button after `p`
 
-$('button:contains("Me?")').after('<div class= "my-div"></div>')
+$('button:contains("Me?")').after('<div class= "my-div"></div>');//adds `div` after `button`
 
 $('button:contains("Me?")').click(function() {
     $('.my-div').append('<p><span>Patrick</span></p>')
-})
+});//creates `p` with `span` inside containing my name in text
+
+$('button:contains("Friends")').click(function() {
+    if (friendCount !== 10) {
+        $('ul').append('<li>' + friends[friendCount] + '</li>');//adds current friend to `ul` as an `li`
+    friendCount++;
+    } else {
+        alert("There are no more friends to list.");
+    };
+});//This will put a friends name into an `li` until there's no one else to list
